35755685f2ff6a601eaf1840ba09aec8a7f69b60
luanaketerlly <luanaketerlly@gmail.com>
2025-07-21 07:10:30 +0000

    Criei o codigo da segunda CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,7 +16,7 @@ int main() {
 
   printf(" Carta 1 \n");
 
-  printf("Digite o código do Estado: \n");
+  printf("Digite a letra do Estado: \n");
   scanf("%c", &Estado);
 
   printf("Digite o Código da Carta: \n");
@@ -41,8 +41,52 @@ int main() {
 
   printf("Carta 1 \n");
   printf("Estado: %c \n Código da carta: %s \n Nome da Cidade: %s \n", Estado, Codigo, Cidade);
-  printf("População: %d \n Área (Km²): %f \n PIB: %f \n Pontos Turísticos: %f \n",Populacao, Area, Pib, Turismo);
+  printf("População: %d \n Área (Km²): %f \n PIB: %f \n Pontos Turísticos: %d \n",Populacao, Area, Pib, Turismo);
+
+  //Grupo de variáveis da segunda carta.
+
+  char Estado2;
+  char Codigo2 [10];
+  char Cidade2 [25];
+  int Populacao2;
+  float Area2;
+  float Pib2;
+  int Turismo2;
+
+   //Início do código de entrada-saída de dados da segunda carta.
+
+    printf(" Carta 2 \n");
+
+    printf("Digite a letra do Estado: \n");
+    scanf("%s", &Estado2);
+
+    printf("Digite o Código da Carta: \n");
+    scanf("%s", &Codigo2);
+
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", &Cidade2);
+
+    printf("Digite o tamanho da população: \n");
+    scanf("%d", &Populacao2);
+
+    printf("Digite o tamanho da área (km²) da Cidade: \n ");
+    scanf("%f", &Area2);
+
+    printf("Digite o valor do PIB da Cidade: \n");
+    scanf("%f", &Pib2);
+
+    printf("Digite a quantidade de pontos turísticos que existem na cidade: \n");
+    scanf("%d", &Turismo2);
+
+
+   //Comando de apresentação dos valores recebidos-armazenados.
+
+    printf("Carta 2 \n");
+    printf("Estado: %s \n Código da carta: %s \n Nome da Cidade: %s \n", Estado2, Codigo2, Cidade2);
+    printf("População: %d \n Área (Km²): %f \n PIB: %f \n Pontos Turísticos: %f \n",Populacao2, Area2, Pib2, Turismo2);
+   
 
 
     return 0;
-}
+
+    }
